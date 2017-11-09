65fdb83dcca27e87aa1b705406932c2caad3eab2
bwimsett <b.wimsett@yahoo.co.uk>
2017-11-09 12:38:49 +0000

    began implementation of basic classes

---
+++ src/StaffMember.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Ben on 09/11/2017.
+ */
+public class StaffMember {
+    private int patientID;
+    private int appointmentDate;
+    private int appointmentTime;
+    private int professionalID;
+}
+
+
