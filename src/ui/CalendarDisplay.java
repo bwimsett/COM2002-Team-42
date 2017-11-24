5e145bc792730f33e2116ada0cfcdbbfe13e4df9
bwimsett <b.wimsett@yahoo.co.uk>
2017-11-24 18:59:07 +0000

    started work on calendar display

---
+++ src/ui/CalendarDisplay.java
@@ -0,0 +1,17 @@
+package ui;
+
+import javax.swing.*;
+
+public class CalendarDisplay extends JPanel {
+
+    private JPanel topRow;
+    private JComboBox<String> staffType = new JComboBox(new String[]{"Dentist","Hygienist"});
+
+    private JPanel calendarPanel;
+
+
+    public CalendarDisplay(){
+
+    }
+
+}
