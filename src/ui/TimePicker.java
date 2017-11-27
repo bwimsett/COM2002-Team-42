2e852e740f9b867e9864d8bdee7fc18a3a3581ee
bwimsett <b.wimsett@yahoo.co.uk>
2017-11-27 14:43:00 +0000

    Complete appointment now fully functional

--- src/ui/TimePicker.java
+++ src/ui/TimePicker.java
@@ -6,7 +6,7 @@ public class TimePicker extends JPanel {
 
     JComboBox<Integer> startHour = new JComboBox(new Integer[]{9,10,11,12,13,14,15,16});
     JComboBox<Integer> startMinutes = new JComboBox(new Integer[]{0,15,30,45});
-    JComboBox<Integer> duration = new JComboBox(new Integer[]{30,45,60,120,180});
+    JComboBox<Integer> duration = new JComboBox(new Integer[]{20,30,45,60,120,180,480});
 
     public TimePicker(){
         this.add(new JLabel("Time"));
