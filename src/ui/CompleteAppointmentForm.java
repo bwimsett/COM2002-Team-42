e019c258d8cf100c8939aa6ea8d4e31cf73e28ec
bwimsett <b.wimsett@yahoo.co.uk>
2017-11-27 14:34:27 +0000

    Complete appointment now fully functional

--- src/ui/CompleteAppointmentForm.java
+++ src/ui/CompleteAppointmentForm.java
@@ -1,8 +1,12 @@
 package ui;
 
+import main.DentalPractice;
 import ui.calendar.CalendarAppointment;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
 
 public class CompleteAppointmentForm extends JDialog {
 
@@ -36,7 +40,10 @@ public class CompleteAppointmentForm extends JDialog {
         lastRow = new JPanel();
 
         cancelButton = new JButton("Cancel");
+        cancelButton.addActionListener(new CompleteAppointmentCancelButtonListener(this));
         completeButton = new JButton("Complete");
+        completeButton.addActionListener(new CompleteAppointmentCompleteButtonListener(this));
+
         lastRow.add(cancelButton);
         lastRow.add(completeButton);
 
@@ -44,5 +51,74 @@ public class CompleteAppointmentForm extends JDialog {
         add(lastRow);
     }
 
+    public CalendarAppointment getAppointment() {
+        return appointment;
+    }
 
+    public JTextField getCostField() {
+        return costField;
+    }
+}
+
+class CompleteAppointmentCancelButtonListener implements ActionListener {
+
+    CompleteAppointmentForm completeAppointmentForm;
+
+    public CompleteAppointmentCancelButtonListener(CompleteAppointmentForm completeAppointmentForm){
+        this.completeAppointmentForm = completeAppointmentForm;
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        completeAppointmentForm.dispose();
+    }
+}
+
+class CompleteAppointmentCompleteButtonListener implements ActionListener{
+
+
+    CompleteAppointmentForm completeAppointmentForm;
+
+
+    public CompleteAppointmentCompleteButtonListener(CompleteAppointmentForm completeAppointmentForm){
+        this.completeAppointmentForm = completeAppointmentForm;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        int cost = Integer.parseInt(completeAppointmentForm.getCostField().getText());
+
+        CalendarAppointment calendarAppointment = DentalPractice.getCalendar().getCalendarDisplay().getCalendarPanel().getSelectedAppointment();
+        calendarAppointment.complete();
+
+        Connection con = DentalPractice.getCon();
+
+        try {
+            Statement statement = con.createStatement();
+
+            Date appointmentDate = calendarAppointment.getDay();
+            Time appointmentStartTime = calendarAppointment.getStartTime();
+            Time appointmentEndTime = calendarAppointment.getEndTime();
+            int appointmentProfessionalID = calendarAppointment.getProfessionalId();
+
+            String query = "UPDATE team042.Appointment " +
+                    "SET team042.Appointment.Completed = 1, team042.Appointment.AppointmentCost = "+cost+" "+
+                    "WHERE team042.Appointment.AppointmentDate = '"+appointmentDate+"' AND " +
+                    "team042.Appointment.AppointmentStartTime = '"+appointmentStartTime+"' AND "+
+                    "team042.Appointment.AppointmentEndTime = '"+appointmentEndTime+"' AND " +
+                    "team042.Appointment.ProfessionalID = '"+appointmentProfessionalID+"';";
+
+            statement.execute(query);
+
+            DentalPractice.getCalendar().getCalendarDisplay().refreshCalendarPanel();
+
+            statement.close();
+
+            completeAppointmentForm.dispose();
+
+        } catch (SQLException e1) {
+            e1.printStackTrace();
+        }
+    }
 }
